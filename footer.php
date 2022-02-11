a00e332b541d67525a4e415a7cfc3906f4b4c4e8
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-02-11 18:13:53 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,29 @@
+
+<!-- ======= Footer ======= -->
+<footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+     
+
+        
+            <div class="footer-info">
+            
+          
+
+          </div>
+ 
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Multi</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/multi-responsive-bootstrap-template/ -->
+        Designed by  Dileep kushwaha 
+      </div>
+    </div>
+  </footer><!-- End Footer -->
