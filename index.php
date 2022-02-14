72b232f09ed43f1e640759976193fb0ac03bef0b
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-02-14 12:05:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -115,7 +115,7 @@ else{
   </div>
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#"><img src="/crud/logo.svg" height="28px" alt=""></a>
+     
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
